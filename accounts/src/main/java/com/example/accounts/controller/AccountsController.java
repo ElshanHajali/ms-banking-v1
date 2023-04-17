776f164477c0e32f9abafe88db109aeb8f4e4f8f
ElshanHajaliyev <75751404+ElshanHajali@users.noreply.github.com>
2023-04-17 22:22:09 +0400

    Cloud config and Service Discovery is removed!

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -1,12 +1,12 @@
 package com.example.accounts.controller;
 
-import com.example.accounts.config.AccountsCloudConfigs;
 import com.example.accounts.dto.response.AccountsResponse;
-import com.example.accounts.dto.response.PropertyResponse;
 import com.example.accounts.service.AccountsService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,19 +15,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AccountsController {
     private final AccountsService accountsService;
-    private final AccountsCloudConfigs accountsCloudConfigs;
 
     @GetMapping("customers/{id}")
     public List<AccountsResponse> getAccountsByCustomer(@PathVariable Long id) {
         return accountsService.getAccountsByCustomer(id);
-    }
-
-    @GetMapping("properties")
-    public PropertyResponse getPropertiesDetails() {
-        return PropertyResponse.builder()
-                .mailDetails(accountsCloudConfigs.getMailDetails())
-                .activeBranches(accountsCloudConfigs.getActiveBranches())
-                .buildVersion(accountsCloudConfigs.getBuildVersion())
-                .msg(accountsCloudConfigs.getMsg()).build();
     }
 }

09d24fec723a13c88c25b350a79a499f700ebc08
ElshanHajaliyev <75751404+ElshanHajali@users.noreply.github.com>
2023-04-18 16:08:14 +0400

    Grafana and Prometheus is configured for the services!

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -2,6 +2,7 @@ package com.example.accounts.controller;
 
 import com.example.accounts.dto.response.AccountsResponse;
 import com.example.accounts.service.AccountsService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import java.util.List;
 public class AccountsController {
     private final AccountsService accountsService;
 
+    @Timed(value = "getAccountsByCustomer.time", description = "Time taken to return Accounts by requested Customer id.")
     @GetMapping("customers/{id}")
     public List<AccountsResponse> getAccountsByCustomer(@PathVariable Long id) {
         return accountsService.getAccountsByCustomer(id);
